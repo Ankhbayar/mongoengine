b57eace99968db4789e87623c8e8d4a61b5fd5c9
Colin Howe <colinthehowe@gmail.com>
2011-05-27 01:54:52 +0800

    Test needs a connection...

--- tests/queryset.py
+++ tests/queryset.py
@@ -1742,6 +1742,9 @@ class QuerySetTest(unittest.TestCase):
 
 class QTest(unittest.TestCase):
 
+    def setUp(self):
+        connect(db='mongoenginetest')
+
     def test_empty_q(self):
         """Ensure that empty Q objects won't hurt.
         """
